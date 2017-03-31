f7431a5065e367a70cb1f7a187c2a63e985723ec
Carl ye NiceEnTa <poiriercarlhugo@gmail.com>
2017-03-30 20:31:55 -0400

    Correction dans le serveur.js

--- serveur.js
+++ serveur.js
@@ -12,39 +12,35 @@ app.use(bodyParser.json())  // pour traiter les données JSON
 var db // variable qui contiendra le lien sur la BD
 
 //Connection avec MongoDB
-MongoClient.connect('mongodb://127.0.0.1:27017/carnet_adresse', (err, database) => {
-	 if (err) return console.log(err)
-	 db = database
-	app.listen(8081, () =>{
-	 console.log('connexion à la BD et on écoute sur le port 8081')
- })
-})
+    MongoClient.connect('mongodb://127.0.0.1:27017/carnet_adresse', (err, database) => {
+	   if (err) return console.log(err)
+	   db = database
+	   app.listen(8081, () =>{
+	   console.log('connexion à la BD et on écoute sur le port 8081')
+     })
+    })
 
 
 // Affichage de la db Adresse avec MongoDB
-app.get('/',  (req, res)  => {
-   console.log('la route route get / = ' + req.url)
+    app.get('/',  (req, res)  => {
+    console.log('la route route get / = ' + req.url)
  
     var cursor = db.collection('adresses').find().toArray(function(err, resultat){
        if (err) return console.log(err)
     // renders index.ejs
     // affiche le contenu de la BD adresses
     res.render('index.ejs', {adresses:resultat})
-    })  
-})
+      })  
+    })
 
 
 
 // Ajout d'un élément dans la db MongoDB
-app.post('/ajouter',  (req, res) => {
+    app.post('/ajouter',  (req, res) => {
+    console.log(req.body._id);
 
-  console.log(req.body._id);
-
-     var ajoutAdresse={};
-    //var 
-
-    if(req.body._id !="")
-    {
+    var ajoutAdresse={};
+    if(req.body._id !=""){
       ajoutAdresse['_id']=ObjectID(req.body._id);
     }
 
@@ -52,27 +48,25 @@ app.post('/ajouter',  (req, res) => {
     ajoutAdresse["prenom"] = req.body.prenom;
     ajoutAdresse["telephone"] = req.body.telephone;
 
-
         db.collection('adresses').save(ajoutAdresse, (err, result) => {
         if (err) return console.log(err)
         console.log('sauvegarder dans la BD')
         res.redirect('/')
       })
-})
+    })
 
 
 
-// detruire
-app.get('/detruire/:id', (req, res) => {
- var id = req.params.id
- console.log(id)
- db.collection('adresses')
- .findOneAndDelete({"_id": ObjectID(req.params.id)}, (err, resultat) => {
+// Destruction d'un élément dans la db MongoDB
+    app.get('/detruire/:id', (req, res) => {
+      var id = req.params.id
+      console.log(id)
+      db.collection('adresses').findOneAndDelete({"_id": ObjectID(req.params.id)}, (err, resultat) => {
 
-if (err) return console.log(err)
- res.redirect('/')  // redirige vers la route qui affiche la collection
- })
-});
+      if (err) return console.log(err)
+      res.redirect('/')  // redirige vers la route qui affiche la collection
+      })
+    });
 
 
 
